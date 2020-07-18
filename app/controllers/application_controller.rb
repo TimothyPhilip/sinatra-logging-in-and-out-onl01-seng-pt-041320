fa0b2063d1b8ddfda2c67890848714d00387dcb0
timothyphilip <philippetimothe@gmail.com>
2020-07-18 12:51:58 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,15 +11,24 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
+    # binding.pry
+    @user = User.find_by(username: params[:username])
+    if @user
+      session[:user_id] = @user.id
+      redirect '/account'
+    end
+    erb :error
 
   end
 
   get '/account' do
 
+    erb :account
   end
 
   get '/logout' do
-
+    session.clear
+    redirect '/'
   end
 
 
--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,17 @@
 class Helpers
+
+    def self.current_user(session)
+        session[:user_id]
+        @user = User.find_by(id: session[:user_id])
+        @user
+    end
+
+    def self.is_logged_in?(session)
+        if session[:user_id]
+            true
+        end
+    end
+
+
+
 end
